201b1c4dad41f1a0e138d100816ad7438107e432
nukopy <nukopy@gmail.com>
2024-08-19 23:26:25 +0900

    refactor: rename integration test module

--- hello/tests/cli.rs
+++ hello/tests/cli.rs
@@ -23,7 +23,8 @@ fn runs() {
 fn true_exits_with_exit_code_0() {
     // テスト項目: true コマンドの終了コードは 0 である
     // given (前提条件):
-    let cmd = Command::cargo_bin("true");
+    let binary_name = "true";
+    let cmd = Command::cargo_bin(binary_name);
     let mut cmd = match cmd {
         Ok(cmd) => cmd,
         Err(err) => panic!("{}", err),
@@ -40,7 +41,8 @@ fn true_exits_with_exit_code_0() {
 fn false_exits_with_exit_code_1() {
     // テスト項目: false コマンドの終了コードは 1 である
     // given (前提条件):
-    let cmd = Command::cargo_bin("false");
+    let binary_name = "false";
+    let cmd = Command::cargo_bin(binary_name);
     let mut cmd = match cmd {
         Ok(cmd) => cmd,
         Err(err) => panic!("{}", err),
