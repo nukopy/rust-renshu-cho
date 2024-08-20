fa8a5f9d5f611db91a5f4966f5f63c72278ac8be
nukopy <nukopy@gmail.com>
2024-08-20 23:15:19 +0900

    feat: impl echo

--- echo/src/main.rs
+++ echo/src/main.rs
@@ -1,3 +1,15 @@
+mod args;
+
+use clap::Parser;
+
+use args::Args;
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    // extract args
+    let text = args.text;
+    let omit_newline = args.omit_newline;
+
+    print!("{}{}", text.join(" "), if omit_newline { "" } else { "\n" });
 }
