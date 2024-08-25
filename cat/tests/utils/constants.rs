13c93d72ba99af4ee87622cd53b28bfa51cb1381
nukopy <nukopy@gmail.com>
2024-08-26 02:10:08 +0900

    feat: add test utils

---
+++ cat/tests/utils/constants.rs
@@ -0,0 +1,10 @@
+#![allow(dead_code)]
+
+// binary name of cat
+pub const BINARY_NAME: &str = "cat";
+
+// input files
+pub const EMPTY: &str = "tests/inputs/empty.txt";
+pub const FOX: &str = "tests/inputs/fox.txt";
+pub const SPIDERS: &str = "tests/inputs/spiders.txt";
+pub const BUSTLE: &str = "tests/inputs/the-bustle.txt";
---
+++ cat/tests/utils/mod.rs
@@ -0,0 +1,3 @@
+pub mod constants;
+pub mod file;
+pub mod run;
