1c3effaba0366b763ef378639984068d02f4d385
nukopy <nukopy@gmail.com>
2024-09-02 01:36:11 +0900

    fix: fix comments & sort options according to orders to print

--- wc/src/args.rs
+++ wc/src/args.rs
@@ -13,16 +13,6 @@ pub struct Args {
     #[clap(value_name = "FILE", help = "Files to read on wc", default_value = "-")]
     pub files: Vec<String>,
 
-    /// wc -c option
-    #[clap(
-        short = 'c',
-        long = "bytes",
-        conflicts_with = "chars", // -c と -m は同時に指定できないようにする
-        help = "Print the number of bytes in each input file",
-        default_value_t = true
-    )]
-    pub bytes: bool,
-
     /// wc -l
     #[clap(
         short = 'l',
@@ -36,11 +26,20 @@ pub struct Args {
     #[clap(
         short = 'w',
         long = "words",
-        // conflicts_with = "bytes", // -n と -c は同時に指定できないようにする
-        help = "Print the number of lines in each input file",
+        help = "Print the number of words in each input file",
         default_value_t = true
     )]
     pub words: bool,
+
+    /// wc -c
+    #[clap(
+        short = 'c',
+        long = "bytes",
+        conflicts_with = "chars", // -c と -m は同時に指定できないようにする
+        help = "Print the number of bytes in each input file",
+        default_value_t = true
+    )]
+    pub bytes: bool,
 
     /// wc -m
     #[clap(
