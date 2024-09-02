8e121c43720093e56ae31a6f8d7225179f7b6efd
nukopy <nukopy@gmail.com>
2024-09-03 04:28:53 +0900

    refactor: clean code

--- uniq/src/lib.rs
+++ uniq/src/lib.rs
@@ -16,6 +16,11 @@ fn print_line(
     count: i32,
     enable_count: bool,
 ) -> Result<(), Box<dyn std::error::Error>> {
+    if count == 0 {
+        // 初回読み込み時の比較は必ず失敗するが、この if ブロックで出力を弾くことができる
+        return Ok(());
+    }
+
     let s = if enable_count {
         format!("{:>4} {}", count, line)
     } else {
@@ -33,39 +38,32 @@ fn uniq(
     enable_count: bool,
 ) -> Result<(), Box<dyn std::error::Error>> {
     let mut prev_line = String::new();
-    let mut next_line = String::new();
+    let mut line = String::new();
+    let mut count = 0;
 
-    // 最初の一行を読み込む
-    let line_bytes = input.read_line(&mut prev_line)?;
-    if line_bytes == 0 {
-        // EOF: input が empty のとき
-        return Ok(());
-    }
-
-    // 二行目以降を読み込む
-    let mut count = 1; // 最初の一行目の分のカウント
     loop {
-        let line_bytes = input.read_line(&mut next_line)?;
+        let line_bytes = input.read_line(&mut line)?;
         if line_bytes == 0 {
-            // EOF のとき、prev の情報を出力してloop を抜ける
+            // EOF のとき、prev の情報を出力して loop を抜ける
             print_line(&mut output, prev_line.clone().as_str(), count, enable_count)?;
             break;
         }
 
-        // ここで prev_line の振る舞いが決まる
-        // 比較のときは行末の改行文字は含まない
-        if prev_line.trim_end() == next_line.trim_end() {
-            count += 1;
-            // このとき、prev_line は更新しない。
-            // 出力されるときにカウントの最初の要素が出力される。
-        } else {
+        // prev と現在の行を比較する
+        // もし等しくない場合、prev_line の集計が終わるので、出力を行い、count をリセットする
+        if prev_line.trim_end() != line.trim_end() {
             // 等しくないときは prev の情報を出力して count をリセットする
             print_line(&mut output, prev_line.clone().as_str(), count, enable_count)?;
-            prev_line = next_line.clone();
-            count = 1;
+            prev_line = line.clone();
+            count = 0;
         }
 
-        next_line.clear();
+        // prev と現在の行が等しい場合は単に count をインクリメント
+        // 等しくなかった場合も、現在の行のカウントを 1 から始めたいのでここでインクリメントして 1 にして次のループへ
+        count += 1;
+
+        // buffer は毎 loop ごとにリセットする
+        line.clear();
     }
 
     Ok(())
